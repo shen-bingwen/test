f7151e16764f9ccfddc20f1575b7735f270d6acc
shen-bingwen <2495103664@qq.com>
2025-01-25 14:30:23 +0800

    修改测试

--- test_1_24/test_1_24.c
+++ test_1_24/test_1_24.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
-int main(){
-    return 0;
+// 数组
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    printf("--start--\n");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    printf("received %d1 %d\n", num1, num2);
+    printf("--END--\n");
+    printf("NEW LINE\n");
 }
